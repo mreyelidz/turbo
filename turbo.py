410d24e783bec290b572e973a93c7f8cb1c76ce4
BongHitz <itz.bonghitz@gmail.com>
2023-07-13 15:32:31 -0400

    Update turbo.py

--- turbo.py
+++ turbo.py
@@ -41,5 +41,6 @@ Post data (64 bytes)
 Type: application/x-www-form-urlencoded
 action=login&userName=admin&passWd=%s&country=N/A&city=N/A''')
 
-# Run the command to execute the turbo-intruder with basic.py and request.txt
+# Install Java docker using this command
+subprocess.run(["docker", "run", "-it", "bellsoft/liberica-openjdk-alpine"])
 subprocess.run(["java", "-jar", "turbo-intruder-all.jar", "basic.py", "request.txt"])
